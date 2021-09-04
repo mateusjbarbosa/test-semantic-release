1f3c97cb40b7e6b13b39fd8f0de2bf941e253e37
Mateus José Barbosa <mateus@findme.id>
2021-09-04 10:04:10 -0300

    refactor: remove whatever test

--- src/Domain/Person/Person.spec.ts
+++ src/Domain/Person/Person.spec.ts
@@ -1,10 +1,6 @@
 import { Person } from "./Person"
 
 describe('Test', () => {
-  test('whatever test', () => {
-    expect(1 + 1).toBe(2)
-  })
-
   test('create person', () => {
     const sut = new Person('Any Name')
 
