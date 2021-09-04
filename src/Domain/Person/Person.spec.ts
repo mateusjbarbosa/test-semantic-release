2a97805017be7a328f6a171c8eb261e5909cfc00
Mateus José Barbosa <mateus@findme.id>
2021-09-04 10:03:06 -0300

    test: ensure get person name

--- src/Domain/Person/Person.spec.ts
+++ src/Domain/Person/Person.spec.ts
@@ -1,5 +1,13 @@
+import { Person } from "./Person"
+
 describe('Test', () => {
-  test('Whatever test', () => {
+  test('whatever test', () => {
     expect(1 + 1).toBe(2)
+  })
+
+  test('create person', () => {
+    const sut = new Person('Any Name')
+
+    expect(sut.getName()).toBe('Any Name')
   })
 })
